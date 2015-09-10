9b3f1ee248efda33e5ad76d3a401822108e15823
Mahesh-sathavalli <snmahesh21@gmail.com>
2015-09-10 23:17:09 +0530

    arraysum

---
+++ Warmup/SimpleArraySum.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num = raw_input()
+array = raw_input()
+array = array.split()
+sum1 = 0
+array = map(int,array)
+print sum(array)
